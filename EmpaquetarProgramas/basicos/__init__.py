4c66d21c0e93c17f5d14612180ed070dece66520
Bryan Anariba <saariel115@gmail.com>
2020-09-14 18:57:15 -0600

    Actualiza el repositorio

--- EmpaquetarProgramas/basicos/__init__.py
+++
@@ -1,1 +0,0 @@
-#con __init__.py se especifica que es un paquete y empaqueta
--- EmpaquetarProgramas/calculosTrigonometrico.py
+++
@@ -1,7 +0,0 @@
-import math
-
-def funcionSeno(numero):
-    print("Valor de sen: " , math.sin(numero))
-
-def funcionPI():
-    print("Valor de numero PI: " , math.pi)
